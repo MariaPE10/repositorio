126aeb7fb7d46ffa1c913e200fffa1de4538b957
Maria <mariaperezescalera@gmail.com>
2019-02-25 10:23:56 +0100

    Mi primer commit

---
+++ operararrays/src/practica4/Arrays.java
@@ -0,0 +1,38 @@
+package practica4;
+
+
+public class Arrays {
+
+	/**
+	 * Método que crea un array con la longitud que le especifique el usuario y lo rellena con valores entre los dos límites,
+	 * también indicados por el usuario.
+	 * @author Maria
+	 * @param longitud: longitud del array.
+	 * @param minimo: valor mínimo que podrá tomar nuestro array.
+	 * @param maximo: valor máximo que podrá tomar nuestro array.
+	 * @return
+	 */
+	public static int[] rellenarArray(int longitud, int minimo, int maximo) {
+		int array[] = new int[longitud];
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i]= (int)Math.round(Math.random()* (maximo-minimo) +  minimo);
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + "  ");
+		}
+		
+		return array;
+	}
+	
+	/**
+	 * Metodo que recibe un array y lo imprime.
+	 * @param array
+	 */
+	public static void imprimeArray(int array[]) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + "  ");
+		}
+	}
+}
